a5ada9b5e1db7548edcdd5976fec42d1ff8637cc
adamocallaghan <adam.ocallaghan@yahoo.ie>
2024-09-25 21:36:43 +0100

    contract outline set up & createOffer function complete

--- src/LiquiMarkets.sol
+++ src/LiquiMarkets.sol
@@ -1,4 +1,49 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract LiquiMarkets {}
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract LiquiMarkets {
+    // ====================
+    // === STORAGE VARS ===
+    // ====================
+
+    struct Offer {
+        uint256 points;
+        uint256 sellerCollateral;
+        uint256 buyerCollateral;
+        Offer_Status status;
+    }
+
+    enum Offer_Status {
+        OPEN,
+        ACCEPTED,
+        SETTLED,
+        DEFAULT
+    }
+
+    Offer[] offers;
+
+    // =======================
+    // === OFFER FUNCTIONS ===
+    // =======================
+
+    function createOffer(uint256 _points) public payable {
+        // create Offer with points & msg.value
+        offers.push(
+            Offer({points: _points, sellerCollateral: msg.value, buyerCollateral: 0, status: Offer_Status.OPEN})
+        );
+    }
+
+    function acceptOffer(uint256 _offerIndex) public payable {}
+
+    function cancelOffer() public {}
+
+    // ============================
+    // === SETTLEMENT FUNCTIONS ===
+    // ============================
+
+    function settle() public {}
+
+    function claimTokens() public {}
+}
